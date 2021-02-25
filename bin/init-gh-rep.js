a73431e98da5adfc077e00a8d7cd7d17a7198cb8
776A0A <240508428@qq.com>
2021-02-25 15:55:54 +0800

    修改bin结构

---
+++ bin/init-gh-rep.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const inquirer = require('inquirer')
+const ora = require('ora')
+const { spawn } = require('child_process')
+const { log } = require('console')
+const { validateEmpty } = require('../utils')
+const chalk = require('chalk')
+const commander = require('commander')
+
+const pkg = require('../package.json')
+
+commander
+    .version(pkg.version, '-v, --version', 'output the current version')
+    .option('-h, --help', 'get usage')
+    .command('init-gh-rep')
+    .description('init a github rep.')
+    .action(ask)
+
+commander.parse(process.argv) 
+
+function ask() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Input a project name.',
+                validate: validateEmpty,
+            },
+            {
+                type: 'input',
+                name: 'commitMessage',
+                message: 'Input the commit message.',
+                default: 'first commit',
+            },
+            {
+                type: 'input',
+                name: 'branchName',
+                message: 'Input a branch name.',
+                default: 'master',
+            },
+            {
+                type: 'input',
+                name: 'address',
+                message: 'Input your project address',
+                validate: validateEmpty,
+            },
+        ])
+        .then(({ projectName, commitMessage, branchName, address }) => {
+            const spinner = ora(`${chalk.yellow('Initializing...')}\n`)
+            spinner.start()
+
+            const cmdLines = `echo "# ${projectName}" >> README.md && git init && git add README.md && git commit -m "${commitMessage}" && git branch -M ${branchName} && git remote add origin git@github.com:${address}.git && git push -u origin ${branchName}`
+
+            spawn(cmdLines, [], {
+                cwd: process.cwd(),
+                stdio: 'inherit',
+                shell: true,
+            })
+                .on('exit', () => {
+                    spinner.succeed('initial succeed!\n')
+                })
+                .on('error', (err) => {
+                    spinner.fail(`${chalk.red('initial failed.')}\n`)
+                    log(err)
+                })
+        })
+}
