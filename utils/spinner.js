b502165fa653633b8a7b3aec155ff956e7f36890
776A0A <240508428@qq.com>
2021-03-05 18:24:46 +0800

    chore: 解决一些冲突

---
+++ utils/spinner.js
@@ -0,0 +1,22 @@
+const ora = require('ora')
+
+class Spinner {
+    start() {
+        return (this.spinner = ora({
+            text: 'Start!\n',
+            spinner: 'dots11',
+        }).start())
+    }
+
+    stop(state, text) {
+        return state
+            ? this.spinner.succeed(text ?? 'Succeed!')
+            : this.spinner.fail(text ?? 'Failed!')
+    }
+
+    set text(text) {
+        this.spinner.text = text
+    }
+}
+
+module.exports = Spinner
