1539e5f6070a37ee576c3d572e48b31be227f87d
776A0A <240508428@qq.com>
2021-03-05 13:58:16 +0800

    feat(sepc-dev): 添加secp-dev下的template

---
+++ scripts/sepc-dev/template/.eslintrc.js
@@ -0,0 +1,37 @@
+const prettierConfig = require('./.prettierrc.js')
+
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['airbnb-base'],
+    plugins: ['prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+    },
+    rules: {
+        'prettier/prettier': [
+            'error',
+            { ...prettierConfig, trailingComma: 'es5' },
+        ],
+        semi: ['error', 'never'], // 不需要不必要的分号
+        'comma-dangle': [
+            'error',
+            {
+                arrays: 'always-multiline',
+                objects: 'always-multiline',
+                imports: 'never',
+                exports: 'never',
+                functions: 'never',
+            },
+        ], // 对于逗号的规则
+        'no-console': 'off', // 允许使用console
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        'jsx-quotes': ['error', 'prefer-single'],
+    },
+}
