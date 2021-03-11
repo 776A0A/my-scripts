60d79557980cd4cb3482d788c939e290e8cd8665
776A0A <har3dy@icloud.com>
2021-03-11 10:50:06 +0800

    feat(bin): bin下的ts改回js

--- bin/index.js
+++ bin/index.js
@@ -2,4 +2,4 @@
 
 'use strict'
 
-require('./exec.js')
+require('./register.js')
--- bin/register.js
+++ bin/register.js
@@ -1,17 +1,16 @@
-import commander from 'commander'
-import pkg from '../package.json'
-import { Commander } from '../src/types'
+const commander = require('commander')
+const pkg = require('../package.json')
 
 commander
     .version(pkg.version, '-v, --version', 'output the current version')
     .option('-h, --help', 'get usage of sns')
 
 // const initGhRep = require('./init-gh-rep')
-import initSpecDev from '../src/scripts/spec-dev'
+const initSpecDev = require('../src/scripts/spec-dev')
 
 runRegisterQueue([initSpecDev])
 
-function runRegisterQueue(registers: Commander[]) {
+function runRegisterQueue(registers) {
     registers.forEach((r) => r.register())
 }
 
