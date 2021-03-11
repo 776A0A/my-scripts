5d3cc20f331fe7dc289ac0ccf81f04cb1a38bde7
776A0A <har3dy@icloud.com>
2021-03-11 10:00:42 +0800

    feat(bin): 在node环境中正确执行ts文件

--- bin/exec.js
+++ bin/exec.js
@@ -1,9 +1,19 @@
-const { exec } = require('child_process')
 const path = require('path')
+const minimist = require('minimist')
+const execa = require('execa')
 
-exec(
-    `npx ts-node "${path.resolve(__dirname, './register.ts')}"`,
-    (error, stdout, stdin) => {
-        console.log(stdout)
-    }
-)
+const argv = []
+
+const options = minimist(process.argv.slice(2))
+
+const commander = options._
+
+if (commander.length > 0) argv.push(`${commander[0]}`)
+if (options.v || options.version) argv.push('-v')
+if (options.h || options.help) argv.push('-h')
+
+execa(`npx ts-node "${path.resolve(__dirname, './register.ts')}"`, argv, {
+    stdio: 'inherit',
+    cwd: __dirname,
+    shell: true,
+})
